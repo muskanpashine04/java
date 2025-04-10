c329af11ea2d26c4c6cc4e88f922024473c153e9
muskanpashine04 <muskanpashine04@gmail.com>
2025-04-10 21:37:40 +0530

    Add files via upload

---
+++ printnumbers.java
@@ -0,0 +1,36 @@
+class printnumbers {
+    public static void main(String [] args){
+        int a=5;
+        switch(a){
+        case 1:
+            System.out.println("one");
+        break;
+        case 2:
+            System.out.println("Two");
+        break;
+        case 3:
+            System.out.println("three");
+            break;
+        case 4:
+            System.out.println("four");
+            break;
+        case 5:
+            System.out.println("Five");
+            break;
+        case 6:
+            System.out.println("Six");
+            break;
+        case 7:
+            System.out.println("Seven");
+            break;
+        default:
+            System.out.println("Invalid input");
+        
+    }
+    }
+
+}
+
+ {
+    
+}
