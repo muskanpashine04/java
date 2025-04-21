82c0b5ae09fb5f745dd4a88d129b722cf353f230
muskanpashine04 <muskanpashine04@gmail.com>
2025-04-21 11:48:04 +0530

    Add files via upload

---
+++ 2dto1d.java
@@ -0,0 +1,29 @@
+public class 2dto1d {
+    
+        public static void main(String[] args){
+            int[][] arr={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}
+            int total=0;
+            int z=0;
+            for(int i=0;i<arr.length;i++){  //rows
+               
+                total+=arr[i].length;
+             //loop will run as many times as number of elements
+                }
+            }
+            System.out.println( total);  
+            int[] arr1=new int[total]
+            for(int k=0;k<arr1.length;k++){
+            for(j=0;j<arr1[k].length;j++){
+            arr[z]=arr[k][j];
+            z++;
+            }
+        }
+
+
+        for(int f=0;f<arr1.length;f++){
+            System.out.print(arr1[f]);
+        }
+        }
+    }
+    
+}
