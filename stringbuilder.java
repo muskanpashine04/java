7c8dcc6940a9f36fea7b58e959ccd9ba46186125
muskanpashine04 <muskanpashine04@gmail.com>
2025-04-26 18:20:58 +0530

    Add files via upload

---
+++ stringbuilder.java
@@ -0,0 +1,8 @@
+public class stringbuilder {
+    public ststic void main(String[] args){
+        StringBuilder str=new StringBuilder("hello"); //mutable string
+        str.append("hii");  //mutable string
+        System.out.println(str); //hellohii
+    }
+    
+}
