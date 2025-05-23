5b9cdc5c8b9c1f9b39401435b31c841211a48d4a
muskanpashine04 <muskanpashine04@gmail.com>
2025-05-23 20:13:28 +0530

    Add files via upload

---
+++ Array__List.java
@@ -0,0 +1,25 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+public class Array__List {
+    public static void main(String[] args) {
+        arr.set("rishabh");
+        System.out.println("array list is"+ arr);
+        //arr.clear();
+        arr.remove(2);
+        System.out.println(arr);
+        arr.add("abhishek");
+         arr.add("muskan");
+          arr.add("addi");
+           arr.add("aaru");
+            arr.add("zehen");
+            collections.sort(arr);
+            System.out.println("array list"+arr);
+    }
+    
+}
+ //output
+ //before change[muskan, rohit,om , madhu]
+//after change[muskan, rohit , om, aditya, madhu]
+//arraylist is[muskan, rohit , om, aditya, madhu]
+//[muskan, rohit, rishabh, madhu]
+//Array List [zehen, aaru, abhishek, addi, muskan , muskan, madhu, rishabh, rohit]
